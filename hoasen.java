f517548e730e26fcd786740d7bce68610d3fd64e
minhhoangbeo <minhhoang.hsu.demo@gmailcom>
2019-06-12 22:44:32 +0700

    lap 5 lan

--- hoasen.java
+++ hoasen.java
@@ -1,8 +1,8 @@
 public class hoasen
 {
-		public static v main(String[] args)
+		public static void main(String[] args)
 		{
-			for(int i =1;i<=10;i++)
+			for(int i =1;i<=5;i++)
 			{
 				System.out.println("Hoa sen..."+i);
 			}
